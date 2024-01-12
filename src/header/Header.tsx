0548b331b5d33df071895fd0ffe42002a508bdce
muky-albaneey <mukyalbani1@gmail.com>
2024-01-12 12:49:36 +0100

    football_frontend

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,14 +1,9 @@
+import { BiLogIn } from "react-icons/bi"; 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { NavLink } from 'react-router-dom';
-import { MdOutlineEventSeat } from "react-icons/md"; 
-import { BsSuitClub } from "react-icons/bs"; 
-import { AiOutlineTrophy } from "react-icons/ai"; 
-import { AiOutlineLineChart } from "react-icons/ai"; 
-import { GiJesterHat } from "react-icons/gi"; 
-import { CgTv } from "react-icons/cg"; 
 import './header.css'
 
 
@@ -30,19 +25,16 @@ function Header() {
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                {/* <Navbar.Brand><NavLink  to='/'>ATHLETIC CLUB MIDLANDS</NavLink></Navbar.Brand> */}
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-           <NavLink to="#action1" style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}> <CgTv />ACM-TV <span>|</span></NavLink>
-          <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}><GiJesterHat />SHOP <span>|</span></NavLink>
-           <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}><AiOutlineLineChart />INVEST <span>|</span></NavLink>
-           <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}><AiOutlineTrophy />TROPHIES <span>|</span></NavLink>
-              <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}><BsSuitClub />KIDSCLUB <span>|</span></NavLink>
-              <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}>MEMBERSHIP <span>|</span></NavLink>
-               <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}><MdOutlineEventSeat />EVENTS <span>|</span></NavLink>
-              <NavLink to="#action2"  style={{color:'rgb(149, 4, 4)',fontWeight: 'bold', marginLeft: '2rem'}}>LOGIN <span>|</span></NavLink>
+           <NavLink to="#action1" className='nav_item'> CLUB</NavLink>
+          <NavLink to="#action2"  className='nav_item'>SHOP</NavLink>
+           <NavLink to="#action2"  className='nav_item'>FAN</NavLink>
+           <NavLink to="#action2"  className='nav_item'>CONTACT US</NavLink>
+              <NavLink to="#action2"  className='nav_item'>ABOUT US</NavLink>                             
+              <NavLink to="#action2" className='nav_item log_nav'>LOGIN <BiLogIn /></NavLink>
                 
             </Nav>
                 
