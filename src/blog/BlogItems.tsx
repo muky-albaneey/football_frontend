5f572a4abac30e603836c42d56ad2d9add991199
muky-albaneey <mukyalbani1@gmail.com>
2024-01-19 18:25:11 +0100

    football_frontend

--- src/blog/BlogItems.tsx
+++ src/blog/BlogItems.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Avatar, List } from 'antd';
-
+import Ellipse_6 from '../assets/Ellipse_6.jpg'
+import Ellipse_7 from '../assets/Ellipse_7.jpg'
+import Ellipse_8 from '../assets/Ellipse_8.jpg'
+import Ellipse_9 from '../assets/Ellipse_9.jpg'
 const data = [
   {
     title: 'This is just a place holder text.',
-    image : '../../public/Ellipse_6.jpg',
+    image : Ellipse_6,
     desc : 'Judit Agu'
   },
   {
     title: 'This is just a place holder text.',
-    image : '../../public/Ellipse_7.jpg',
+    image : Ellipse_7,
     desc : 'Brown Salami'
   },
   {
     title: 'This is just a place holder text.',
-    image : '../../public/Ellipse_8.jpg',
+    image : Ellipse_8,
     desc : 'Tunde Ololade'
   },
   {
     title: 'This is just a place holder text.',
-    image : '../../public/Ellipse_9.jpg',
+    image : Ellipse_9,
     desc : 'Kolada Da-silva'
   },
 ];
