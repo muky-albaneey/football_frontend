dc31005c61fef66324ea7ceab7668901ef029e68
muky-albaneey <mukyalbani1@gmail.com>
2024-01-23 10:20:12 +0100

    football_frontend

--- src/feature/Features.tsx
+++ src/feature/Features.tsx
@@ -1,59 +1,68 @@
 import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
-import './feature.css'
+import './feature.css';
+
 function Features() {
   return (
-   <section id="featureWrapper">
-         <Carousel id='carouselFeature'>
-      <Carousel.Item id='featureItem'>
-      <div className="leftFeatureMatcHeader">
-                <h1>MATCHES</h1>
-                <h5>NLO</h5>
-                <h5>MATCHDAY 12</h5>
-                <h5>2 February 2024</h5>
-                <h5>16:30</h5>
-            </div>
-        <main id="featureItemContent">
-          
-            <div className="leftFeatureMatchDetails"></div>
-            <div className="centerFeatureMatchDetails"></div>
-            <div className="rightFeatureMatchDetails"></div>
-        </main>
-        <div className="leftFeatureMatcFooter">
+    <section id="featureWrapper">
+      <Carousel id='carouselFeature' controls prevIcon={<CustomPrevIcon />} nextIcon={<CustomNextIcon />}>
+        <Carousel.Item id='featureItem'>
+          <div className="leftFeatureMatcHeader">
+            <h1>MATCHES</h1>
+            <h5>NLO</h5>
+            <h4>MATCHDAY 12</h4>
+            <h5>2 February 2024</h5>
+            <h5>16:30</h5>
+          </div>
+          <main id="featureItemContent">
+            <div className="leftFeatureMatchDetails fetureDetails"></div>
+            <div className="centerFeatureMatchDetails fetureDetails"></div>
+            <div className="rightFeatureMatchDetails fetureDetails"></div>
+          </main>
+          <div className="leftFeatureMatcFooter">
             see all
-        </div>
-      </Carousel.Item>
-      <Carousel.Item id='featureItem'>    
-      {/* <img
-           className="d-block w-100"
-             src='./wallpaperflare_2.jpg'
-           alt="First slide"
-         />     */}
-           {/* <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item id='featureItem'>    
-      {/* <img
-           className="d-block w-100"
-             src='./wallpaperflare_2.jpg'
-           alt="First slide"
-         />     */}
-           {/* <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-    </Carousel>
-   </section>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item id='featureItem'>
+          <div className="leftFeatureMatcHeader">
+            <h1>MATCHES</h1>
+            <h5>NLO</h5>
+            <h4>MATCHDAY 12</h4>
+            <h5>2 February 2024</h5>
+            <h5>16:30</h5>
+          </div>
+          <main id="featureItemContent">
+            <div className="leftFeatureMatchDetails fetureDetails"></div>
+            <div className="centerFeatureMatchDetails fetureDetails"></div>
+            <div className="rightFeatureMatchDetails fetureDetails"></div>
+          </main>
+          <div className="leftFeatureMatcFooter">
+            see all
+          </div>
+        </Carousel.Item>
+        <Carousel.Item id='featureItem'>
+          <div className="leftFeatureMatcHeader">
+            <h1>MATCHES</h1>
+            <h5>NLO</h5>
+            <h4>MATCHDAY 12</h4>
+            <h5>2 February 2024</h5>
+            <h5>16:30</h5>
+          </div>
+          <main id="featureItemContent">
+            <div className="leftFeatureMatchDetails fetureDetails"></div>
+            <div className="centerFeatureMatchDetails fetureDetails"></div>
+            <div className="rightFeatureMatchDetails fetureDetails"></div>
+          </main>
+          <div className="leftFeatureMatcFooter">
+            see all
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </section>
   );
 }
+
+// CustomPrevIcon and CustomNextIcon can be styled components or SVGs representing your custom icons
+const CustomPrevIcon = () => <span style={{ fontSize: '24px', color: 'rgb(149, 4, 4)', marginBottom: '-15rem',  }}>&lt;</span>;
+const CustomNextIcon = () => <span style={{ fontSize: '24px', color: 'rgb(149, 4, 4)', marginBottom: '-15rem',  }}>&gt;</span>;
 
 export default Features;
