ad77dc15b746d245b0b8ad9efde9c370f12a2687
muky-albaneey <mukyalbani1@gmail.com>
2024-01-23 10:51:41 +0100

    football_frontend

--- src/feature/Features.tsx
+++ src/feature/Features.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Features() {
   return (
     <section id="featureWrapper">
-      <Carousel id='carouselFeature' controls prevIcon={<CustomPrevIcon />} nextIcon={<CustomNextIcon />}>
+      <Carousel id='carouselFeature' controls prevIcon={<CustomPrevIcon />} nextIcon={<CustomNextIcon />} interval={19000} fade indicators={false}>
         <Carousel.Item id='featureItem'>
           <div className="leftFeatureMatcHeader">
             <h1>MATCHES</h1>
@@ -15,9 +15,19 @@ function Features() {
             <h5>16:30</h5>
           </div>
           <main id="featureItemContent">
-            <div className="leftFeatureMatchDetails fetureDetails"></div>
-            <div className="centerFeatureMatchDetails fetureDetails"></div>
-            <div className="rightFeatureMatchDetails fetureDetails"></div>
+            <div className="leftFeatureMatchDetails fetureDetails">
+            <img src='./willbro.jpg' alt="" className="featuresImg"/>     
+
+            </div>
+            <div className="centerFeatureMatchDetails fetureDetails">
+                <h1>VS</h1>
+                <button className="featureTicketBtn">
+                    buy tikect
+                </button>
+            </div>
+            <div className="rightFeatureMatchDetails fetureDetails">
+            <img src='./Frame.jpg' alt="" className="featuresImg"/>    
+            </div>
           </main>
           <div className="leftFeatureMatcFooter">
             see all
@@ -32,9 +42,19 @@ function Features() {
             <h5>16:30</h5>
           </div>
           <main id="featureItemContent">
-            <div className="leftFeatureMatchDetails fetureDetails"></div>
-            <div className="centerFeatureMatchDetails fetureDetails"></div>
-            <div className="rightFeatureMatchDetails fetureDetails"></div>
+            <div className="leftFeatureMatchDetails fetureDetails">
+            <img src='./willbro.jpg' alt="" className="featuresImg"/>     
+
+            </div>
+            <div className="centerFeatureMatchDetails fetureDetails">
+                <h1>VS</h1>
+                <button className="featureTicketBtn">
+                    buy tikect
+                </button>
+            </div>
+            <div className="rightFeatureMatchDetails fetureDetails">
+            <img src='./Frame.jpg' alt="" className="featuresImg"/>    
+            </div>
           </main>
           <div className="leftFeatureMatcFooter">
             see all
@@ -49,12 +69,22 @@ function Features() {
             <h5>16:30</h5>
           </div>
           <main id="featureItemContent">
-            <div className="leftFeatureMatchDetails fetureDetails"></div>
-            <div className="centerFeatureMatchDetails fetureDetails"></div>
-            <div className="rightFeatureMatchDetails fetureDetails"></div>
+            <div className="leftFeatureMatchDetails fetureDetails">
+            <img src='./willbro.jpg' alt="" className="featuresImg"/>     
+
+            </div>
+            <div className="centerFeatureMatchDetails fetureDetails">
+                <h1>VS</h1>
+                <button className="featureTicketBtn">
+                    buy tikect
+                </button>
+            </div>
+            <div className="rightFeatureMatchDetails fetureDetails">
+            <img src='./Frame.jpg' alt="" className="featuresImg"/>    
+            </div>
           </main>
           <div className="leftFeatureMatcFooter">
-            <Link to=''>see all</Link>
+            see all
           </div>
         </Carousel.Item>
       </Carousel>
@@ -63,7 +93,7 @@ function Features() {
 }
 
 // CustomPrevIcon and CustomNextIcon can be styled components or SVGs representing your custom icons
-const CustomPrevIcon = () => <span style={{ fontSize: '24px', color: 'rgb(149, 4, 4)', marginBottom: '-15rem',  }}>&lt;</span>;
-const CustomNextIcon = () => <span style={{ fontSize: '24px', color: 'rgb(149, 4, 4)', marginBottom: '-15rem',  }}>&gt;</span>;
+const CustomPrevIcon = () => <span style={{ fontSize: '24px', color: 'rgb(149, 4, 4)', marginBottom: '-15rem', fontWeight: 'bold' }}>&lt;</span>;
+const CustomNextIcon = () => <span style={{ fontSize: '24px', color: 'rgb(149, 4, 4)', marginBottom: '-15rem', fontWeight: 'bold' }}>&gt;</span>;
 
 export default Features;
