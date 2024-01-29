b7b762d00b7516a8b13f30f1ea7abc0433c11c55
muky-albaneey <mukyalbani1@gmail.com>
2024-01-29 20:47:24 +0100

    football_frontend

--- src/blog/Blogging.tsx
+++ src/blog/Blogging.tsx
@@ -1,17 +1,33 @@
+import Fixture from './Fixture';
 import Paginations from './Pagination'
-// import Card from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
 import './pagination.css';
 // import { NavLink, Outlet } from 'react-router-dom';
-// import Frame_blog_fix from '../assets/Frame_blog_fix.jpg'
+import Frame_blog_fix from '../assets/Frame_blog_fix.jpg'
 
 const Blogging = () => {
-    // const active = {
-    //     color: 'red',
-    //     textDecoration: 'underline'
-    // }
+ 
   return (
-    <section className='think'>
-       
+    <section className='bloggingOverallCon'>
+       <main id="topPagination">
+        <div className="topPaginationLeft">
+        <Card className='cardPAgination cardPAgination_2'>
+          <Card.Img variant="top" src={Frame_blog_fix} />
+          <Card.Body>
+            {/* <Card.Title>Update on player injury</Card.Title> */}
+            <Card.Text>
+              <h1> Update on player injury</h1><br />
+              Men
+            </Card.Text>
+            
+          </Card.Body>
+        </Card>
+        </div>
+        <hr />
+        <div className="topPaginationRight">
+          <Fixture />
+        </div>
+       </main>
       <main id="bottomPAgination">
       <Paginations />
       </main>
--- src/blog/Fixture.tsx
+++ src/blog/Fixture.tsx
@@ -2,23 +2,40 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import './pagination.css';
 import willbro from '../assets/willbro.jpg';
 import Frame from '../assets/Frame.jpg';
+import { NavLink } from "react-router-dom";
+
 
 const Fixture = () => {
+     const active = {
+        color: 'red',
+        textDecoration: 'underline'
+    }
   return (
     <section className='fixture'>
-      <div className="headerPagination  paginatioItem">
+
+      <div className="headerPagination  paginationItem">
+          <header className="fixtureHeader">
+             
+          <NavLink to={`.`} end style={({ isActive }) => isActive ? active : {color:'black'}}>
+          Men
+        </NavLink>
+        
+            <NavLink to={`woman`} style={({isActive}) => isActive ? active :  {color:'black'}}>Women </NavLink>
+          </header>
+       <div className="allFixtures">
         <span>NEXT FIXTURES</span>
         <span>All  fixtures <AiOutlineArrowRight /></span>
+       </div>
       </div>
-      <div className="centerPagination paginatioItem" >
+      <div className="centerPagination paginationItem" >
         <h4>NLO </h4>
         <h4>MATCHDAY 12</h4>
         <h6>2 February 2024</h6>
       </div>
-      <div className="bottomPagination  paginatioItem">
+      <div className="bottomPagination  paginationItem">
         <img src={Frame} alt="" className="featuresImg imgAway"/>
         <button className="paginationBtn">
-        16;30 <br />
+        4:30 <br />
         WAT
         </button>
         <img src={willbro} alt="" className="featuresImg imgAway" />
