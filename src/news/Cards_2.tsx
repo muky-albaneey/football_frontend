fd5b45122217bcb74e58a6100a4a732d651c20bc
muky-albaneey <mukyalbani1@gmail.com>
2024-02-02 09:32:35 +0100

    football_frontend

--- src/news/Cards_2.tsx
+++ src/news/Cards_2.tsx
@@ -11,7 +11,7 @@ function BasicExample() {
         <Card.Title id='cardTitle'> ACM FANS MAKE DONATION FOR PLATEAU VICTIMS</Card.Title>
         <Card.Text  id='cardTextDesc'>
         The images and news from the attacked areas continue to leave us deeply
-concerned. For this reason, the ACM Fan Council are organizing a fundraising campaign at short notice and would like to address the               following      appeal to the ACM fan community...
+        concerned. For this reason, the ACM Fan Council are organizing a fundraising campaign at short notice and would like to address the               following      appeal to the ACM fan community...
         </Card.Text>
         <Button id='btnNews' >
           <Link to='/'  style={{color: 'whitesmoke', textDecoration: 'none'}}>CLICK TO SEE ALL ACM NEWS </Link>
